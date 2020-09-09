d6a3d4cf8c2f12d6e60d20e295d6bf5f1d6e6449
jaimevepe <jaime.velazquez@g.austincc.edu>
2020-09-08 22:28:37 -0500

    Fnished setting up sendGrid, email wont send

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const MyApi = process.env.SENDGRID_API_KEY;
+
 const sendGrid = require('@sendgrid/mail');
+const { send } = require('@sendgrid/mail');
 
 const app = express();
 
@@ -25,6 +28,31 @@ app.use((req, res, next) => {
 
 app.get('/api', (req, res, next) => {
     res.send('API status: Running')
+})
+
+app.post('/api/email', (req, res ,next) => {
+    sendGrid.setApiKey(`${SENDGRID_API_KEY}`);
+    const msg = {
+        to: 'jaimevepe@gmail.com',
+        from: req.body.email,
+        subject: 'Website Contact',
+        text: req.body.message
+    }
+
+    sendGrid.send(msg)
+        .then(result => {
+            res.status(200).json({
+                success: true
+            })
+        })
+        .catch(err => {
+
+            console.log("error: ", err)
+            res.status(401).json({
+                success: false
+            })
+
+        })
 })
 
 app.listen(port, () => {
