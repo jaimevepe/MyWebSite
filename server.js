59f03c66e0d28cfa26c153be10d77d343a3849fa
jaimevepe <jaime.velazquez@g.austincc.edu>
2020-09-08 21:05:52 -0500

    deleted app.get from server

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    console.log()
-})
+
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
