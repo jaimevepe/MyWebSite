4a9c909dafe930b72d95ecaedb5947896e0f722c
jaimevepe <jaime.velazquez@g.austincc.edu>
2020-09-08 21:02:55 -0500

    First commit

---
+++ portfolio/src/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Content from '../components/Content';
+
+function About(props) {
+    return(
+        <div>
+            <Hero title={props.title} />
+            <Content>
+                <p>Hello, my name is Jaime. I'm a full stack developer with experience in ReactJS, Express JS, Node JS, PostgreSQL and Mongo DB</p>
+
+                <p>My Dream is to one day get a job as a Software Engineer (currently working on some projects to build my portfolio even further). </p>
+                
+                <p>I'm Contantly learning new things. Currently those things include gaining more experience with React, SQL, Mongo DB, Express Js and Node JS.
+                   Also getting better at using Google when ever a difficult challenge comes my way. 
+                </p>
+
+                <p>LATEST PROJECT PARAGRAPH WILL GO HERE.</p>
+                 
+                
+            </Content>
+        </div>
+    );
+}
+
+export default About;
