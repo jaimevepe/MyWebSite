d2332a79483dfe2b54a359664f5e1e3ba1583f95
jaimevepe <jaime.velazquez@g.austincc.edu>
2020-12-23 13:27:34 -0600

    merged servermjs for rebase

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 const MyApi = process.env.SENDGRID_API_KEY;
 
 const sendGrid = require('@sendgrid/mail');
@@ -10,6 +11,12 @@ const { send } = require('@sendgrid/mail');
 =======
 const sendGrid = require('@sendgrid/mail');
 >>>>>>> ed0cc41... Started working on server, added body-parser and cors and got the route to /api going
+=======
+const MyApi = process.env.SENDGRID_API_KEY;
+
+const sendGrid = require('@sendgrid/mail');
+const { send } = require('@sendgrid/mail');
+>>>>>>> 9158661... Fnished setting up sendGrid, email wont send
 
 const app = express();
 
@@ -77,6 +84,31 @@ app.use((req, res, next) => {
 app.get('/api', (req, res, next) => {
     res.send('API status: Running')
 >>>>>>> ed0cc41... Started working on server, added body-parser and cors and got the route to /api going
+})
+
+app.post('/api/email', (req, res ,next) => {
+    sendGrid.setApiKey(`${SENDGRID_API_KEY}`);
+    const msg = {
+        to: 'jaimevepe@gmail.com',
+        from: req.body.email,
+        subject: 'Website Contact',
+        text: req.body.message
+    }
+
+    sendGrid.send(msg)
+        .then(result => {
+            res.status(200).json({
+                success: true
+            })
+        })
+        .catch(err => {
+
+            console.log("error: ", err)
+            res.status(401).json({
+                success: false
+            })
+
+        })
 })
 
 app.listen(port, () => {
